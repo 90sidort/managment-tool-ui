4f44c7f07e17e66f76d7b75d0e462201ac166da6
andrew <90sidort@gmail.com>
2021-03-23 15:29:15 +0100

    added update job

--- src/components/job/job.edit.jsx
+++ src/components/job/job.edit.jsx
@@ -28,6 +28,7 @@ export default class JobDetails extends React.Component {
     };
     this.onCompanySelectedHandler = this.onCompanySelectedHandler.bind(this);
     this.onValueChange = this.onValueChange.bind(this)
+    this.onSubmitHandle = this.onSubmitHandle.bind(this)
   } 
 
   componentDidMount() {
@@ -52,8 +53,120 @@ export default class JobDetails extends React.Component {
     }
   }
 
+  onCompanySelectedHandler(e) {
+    this.loadRep(e.target.value);
+    this.loadLoc(e.target.value);
+    this.setState({ companyValue: e.target.value });
+  }
+
+  onValueChange(e) {
+    const value = e.target.value
+    if (e.target.name === "skills") {
+      let add = Array.from(e.target.selectedOptions, option => option.value);
+      this.setState({skills: add});      
+    }
+    else if (e.target.name === "start" || e.target.name === "end") {
+      this.setState({[e.target.name]: new Date(e.target.value)})
+    }
+    else {
+      this.setState({
+        ...this.state,
+        [e.target.name]: value
+      });
+    }
+  }
+
+  onSubmitHandle(e) {
+    e.preventDefault();
+    const form = document.forms.jobEdit;
+    const job = {
+      company: form.company.value,
+      representative: form.repValue.value,
+      location: form.locValue.value,
+      title: form.title.value,
+      personel: parseInt(form.personel.value),
+      rate: parseFloat(form.rate.value),
+      currency: form.currency.value,
+      description: form.description.value,
+      // skills: Array.from(form.skills.selectedOptions, option => option.value),
+      status: 'New',
+      start: new Date(form.start.value),
+      end: new Date(form.end.value),
+      created: new Date(),
+    };
+    console.log(job);
+
+    this.updateData(this.state.id, job)
+  }
+
+  createOptions(name, element) {
+    const options = [];
+    const optionArr = element
+    for (let i = 0; i < optionArr.length; i++) {
+      options.push(
+        <option key={i} value={optionArr[i]._id}>
+          {name === 'companies' && `${optionArr[i].name}`}
+          {name === "skills" && `${optionArr[i].name}`}
+          {name === 'rep' && `${optionArr[i].name}, ${optionArr[i].email}`}
+          {name === 'loc' && `${optionArr[i].city}, ${optionArr[i].country}, ${optionArr[i].address}, ${optionArr[i].postcode}`}
+        </option>
+      );
+    }
+    if (name === "companies") {
+      options.push(
+        <option key={-1} value={-1} disabled>
+          Select company
+        </option>
+      );
+    }
+    return options;
+  }
+
+  async updateData(_id, changes) {
+    const query = `
+      mutation updateJob($_id: ID, $changes: JobInput!){
+        updateJob(_id: $_id, changes: $changes) {
+        title
+        rate
+        currency
+        }
+      }
+    `
+    const data = await graphQLFetch(query, {_id, changes});
+    if (data) {
+      console.log('działa kurła');
+    }
+  }
+
+  async loadCompany() {
+    const query = `
+    query {
+      company {
+        _id
+        name
+      }
+    }`;
+    const data = await graphQLFetch(query);
+    if (data) {
+      this.setState({ companies: data.company })
+    }
+  }
+  async loadSkills() {
+    const query = `
+    query getSkills {
+      skill {
+        name
+      }
+    }`
+    const data = await graphQLFetch(query);
+    if (data) {
+      this.setState({ availSkills: data.skill })
+    }
+  }
+
   async loadRep(cid) {
-    const query = `query getRep($cid: ID) {
+    const query = `
+    query getRep($cid: ID) {
       representative(cid: $cid) {
         _id
         cid
@@ -69,7 +182,8 @@ export default class JobDetails extends React.Component {
   }
 
   async loadLoc(cid) {
-    const query = `query getLocations($cid: ID) {
+    const query = `
+    query getLocations($cid: ID) {
       location(cid: $cid) {
         _id
         cid
@@ -82,73 +196,6 @@ export default class JobDetails extends React.Component {
     const data = await graphQLFetch(query, { cid });
     if (data) {
       this.setState({ locations: data.location });
-    }
-  }
-
-  onCompanySelectedHandler(e) {
-    this.loadRep(e.target.value);
-    this.loadLoc(e.target.value);
-    this.setState({ companyValue: e.target.value });
-  }
-
-  onValueChange(e) {
-    const value = e.target.value
-    if (e.target.name === "skills") {
-      let add = Array.from(e.target.selectedOptions, option => option.value);
-      this.setState({skills: add});      
-    }
-    else if (e.target.name === "start" || e.target.name === "end") {
-      console.log(e.target.value, e.target.name);
-      this.setState({[e.target.name]: new Date(e.target.value)})
-    }
-    else {
-      this.setState({
-        ...this.state,
-        [e.target.name]: value
-      });
-    }
-  }
-
-  createCompItems() {
-    const options = [];
-    const comps = this.state.companies;
-    for (let i = 0; i < comps.length; i++) {
-      options.push(
-        <option key={i} value={comps[i]._id}>
-          {comps[i].name}
-        </option>
-      );
-    }
-    options.push(
-      <option key={-1} value={-1} disabled>
-        Select company
-      </option>
-    );
-    return options;
-  }
-
-  async loadCompany() {
-    const query = `query {
-      company {
-        _id
-        name
-      }
-    }`;
-    const data = await graphQLFetch(query);
-    if (data) {
-      this.setState({ companies: data.company })
-    }
-  }
-  async loadSkills() {
-    const query = `query getSkills {
-      skill {
-        name
-      }
-    }`
-    const data = await graphQLFetch(query);
-    if (data) {
-      console.log(data.skill);
-      this.setState({ availSkills: data.skill })
     }
   }
 
@@ -175,7 +222,6 @@ export default class JobDetails extends React.Component {
     const data = await graphQLFetch(query, { _id });
     if (data) {
       const setData = data.job[0]
-      console.log(3210, setData);
       this.setState({jobId: setData._id})
       this.setState({companyValue: setData.company._id})
       this.setState({repValue: setData.representative._id})
@@ -189,46 +235,6 @@ export default class JobDetails extends React.Component {
       this.setState({start: setData.start})
       this.setState({end: setData.end})
     }
-  }
-
-
-  createSkillsItems() {
-    const options = [];
-    const skills = this.state.availSkills;
-    for (let i = 0; i < skills.length; i++) {
-      options.push(
-        <option key={i} value={skills[i]._id}>
-          {skills[i].name}
-        </option>
-      );
-    }
-    return options;
-  }
-
-  createRepItems() {
-    const options = [];
-    const reps = this.state.representatives;
-    for (let i = 0; i < reps.length; i++) {
-      options.push(
-        <option key={i} value={reps[i]._id}>
-          {reps[i].name}, {reps[i].email}
-        </option>
-      );
-    }
-    return options;
-  }
-
-  createLocItems() {
-    const options = [];
-    const locs = this.state.locations;
-    for (let i = 0; i < locs.length; i++) {
-      options.push(
-        <option key={i} value={locs[i]._id}>
-          {locs[i].city}, {locs[i].country}, {locs[i].address}, {locs[i].postcode}
-        </option>
-      );
-    }
-    return options;
   }
 
   render() {
@@ -254,22 +260,40 @@ export default class JobDetails extends React.Component {
     return(
       <div>
       {jobId ? (
-        <div>
+      <div>
         <Link to={`/details/${this.state.id}`}>Go back</Link>
-        <h3>Select company:</h3>
-        {companies && companyValue &&
-          <select
-            name="company"
-            id="company"
-            value={companyValue}
-            onChange={this.onCompanySelectedHandler}
-          >
-            {this.createCompItems()}
-          </select>}
+        <form name="jobEdit" onSubmit={this.onSubmitHandle}>
+          <h3>Select company:</h3>
+            {companies && companyValue &&
+              <select
+                name="company"
+                id="company"
+                value={companyValue}
+                onChange={this.onCompanySelectedHandler}
+              >
+                {this.createOptions("companies", companies)}
+              </select>
+            }
           <h3>Select representative:</h3>
-          {representatives && repValue && <select name="repValue" value={repValue} onChange={this.onValueChange}>{this.createRepItems()}</select>}
+            {representatives && repValue &&
+              <select
+                name="repValue"
+                value={repValue}
+                onChange={this.onValueChange}
+              >
+                {this.createOptions("rep", representatives)}
+              </select>
+            }
           <h3>Select location:</h3>
-          {locations && locValue && <select name="locValue" value={locValue} onChange={this.onValueChange}>{this.createLocItems()}</select>}
+            {locations && locValue &&
+              <select
+                name="locValue"
+                value={locValue}
+                onChange={this.onValueChange}
+              >
+                {this.createOptions("loc", locations)}
+              </select>
+            }
           <h3>Provide offer title</h3>
             <input
               type="text"
@@ -296,21 +320,38 @@ export default class JobDetails extends React.Component {
               onChange={this.onValueChange}
             />
           <h3>Provide currency:</h3>
-            <select name="currency" value={currency} onChange={this.onValueChange}>
+            <select
+              name="currency"
+              value={currency}
+              onChange={this.onValueChange}
+            >
               <option value="PLN">PLN</option>
               <option value="EUR">EUR</option>
               <option value="GBP">GBP</option>
             </select>
           <h3>Provide description:</h3>
-            <textarea name="description" value={description} onChange={this.onValueChange} />
+            <textarea
+              name="description"
+              value={description}
+              onChange={this.onValueChange}
+            />
           <h3>Provide skills:</h3>
             {availSkills && 
-              <select name="skills" value={skills} onChange={this.onValueChange} multiple>
-                {this.createSkillsItems()}
+              <select
+                name="skills"
+                value={skills}
+                onChange={this.onValueChange}
+                multiple
+              >
+                {this.createOptions("skills", availSkills)}
               </select>
             }
           <h3>Define status:</h3>
-            <select name="status" value={status} onChange={this.onValueChange}>
+            <select
+              name="status"
+              value={status}
+              onChange={this.onValueChange}
+            >
               <option value="New">New</option>
               <option value="Assigned">Assigned</option>
               <option value="Negotiation">Negotiation</option>
@@ -319,11 +360,25 @@ export default class JobDetails extends React.Component {
               <option value="Closed">Closed</option>
             </select>
           <h3>Define start date:</h3>
-            {<input type="date" name="start" value={start} onChange={this.onValueChange} />}
+            <input
+              type="date"
+              name="start"
+              value={start}
+              onChange={this.onValueChange}
+            />
           <h3>Define end date:</h3>
-            <input type="date" name="end" value={end} onChange={this.onValueChange} />
+            <input
+              type="date"
+              name="end"
+              value={end}
+              onChange={this.onValueChange}
+            />
+          <button type="submit">Save</button>
+        </form>
       </div>
-      ) : (<p>Job with this id not found.</p>)  
+      ) : (
+        <p>Job with this id not found.</p>
+      )  
     }
     </div>
   )}

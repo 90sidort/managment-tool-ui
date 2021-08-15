4c82cd09cc1a592773e8de6af321d8d3a3802de8
andrew <90sidort@gmail.com>
2021-08-15 18:55:26 +0200

    add fixtures

--- src/components/toast.wrapper.jsx
+++ src/components/toast.wrapper.jsx
@@ -6,7 +6,9 @@ export default function withToast(OriginalComponent) {
     constructor(props) {
       super(props);
       this.state = {
-        toastVisible: false, toastMessage: '', toastType: 'success',
+        toastVisible: false,
+        toastMessage: '',
+        toastType: 'success',
       };
       this.showSuccess = this.showSuccess.bind(this);
       this.showError = this.showError.bind(this);
@@ -35,11 +37,7 @@ export default function withToast(OriginalComponent) {
             dismissToast={this.dismissToast}
             {...this.props}
           />
-          <Toast
-            bsStyle={toastType}
-            showing={toastVisible}
-            onDismiss={this.dismissToast}
-          >
+          <Toast bsStyle={toastType} showing={toastVisible} onDismiss={this.dismissToast}>
             {toastMessage}
           </Toast>
         </React.Fragment>
